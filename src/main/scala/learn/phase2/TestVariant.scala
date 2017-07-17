51d4b6402f30e6584e85f892912ecf343d3d8ef8
xiaoyue26 <296671657@qq.com>
2017-07-17 15:50:13 +0800

    learn variant2

--- src/main/scala/learn/phase2/TestVariant.scala
+++ src/main/scala/learn/phase2/TestVariant.scala
@@ -4,15 +4,26 @@ package learn.phase2
   * Created by xiaoyue26 on 17/7/17.
   */
 object TestVariant extends App {
+
+  sealed abstract class Car() {}
+
+  class Volvo() extends Car {}
+
+  class VolvoWagon() extends Volvo
+
   // +A 表示接受向上转型
   case class Item[+A](a: A) {
     def get: A = a
   }
+  // -A 表示接受向下转型
+  case class Check[-A]() {
+    def check(a: A) {}
+  }
 
-  abstract class Car() {}
+  val c: Item[Car] = new Item[Volvo](new Volvo) // 用父类指针接了子类
+  println("pass item")
 
-  case class Volvo() extends Car {}
+  val d:Check[VolvoWagon]= new Check[Volvo]() // 用子类指针接了父类
+  println("pass check")
 
-  val c: Item[Car] = Item[Volvo](Volvo())
-  val auto = c.get
 }
