7f1b6bce37b9cc41e7010c2bcaf5d114c9fe6d17
xiaoyue26 <296671657@qq.com>
2017-07-17 11:12:53 +0800

    learn IO

---
+++ src/main/scala/learn/TestIO.scala
@@ -0,0 +1,35 @@
+package learn
+
+import java.io.{File, PrintWriter}
+
+import scala.io.{Source, StdIn}
+
+/**
+  * Created by xiaoyue26 on 17/7/17.
+  */
+object TestIO {
+  def testFileIO(): Unit = {
+    val writer = new PrintWriter(new File("test.out"))
+    writer.write("output from TestIO")
+    writer.close()
+  }
+
+  def testConsole(): Unit = {
+    println("start your input:")
+    val line = StdIn.readLine()
+    println("receive: " + line)
+
+  }
+
+  def testReadFile(): Unit = {
+    Source.fromFile("test.out").foreach {
+      print
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    //testConsole()
+    testReadFile()
+
+  }
+}
