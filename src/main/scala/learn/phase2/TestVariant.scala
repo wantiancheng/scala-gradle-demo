488a4063ada34d1820d6a2a42ab8d18b96540b03
xiaoyue26 <296671657@qq.com>
2017-07-17 16:31:41 +0800

    learn variant3

--- src/main/scala/learn/phase2/TestVariant.scala
+++ src/main/scala/learn/phase2/TestVariant.scala
@@ -10,7 +10,7 @@ object TestVariant extends App {
   class Volvo() extends Car {}
 
   class VolvoWagon() extends Volvo
-
+  // 泛型中默认是强类型,不允许上下转型
   // +A 表示接受向上转型
   case class Item[+A](a: A) {
     def get: A = a
@@ -26,4 +26,5 @@ object TestVariant extends App {
   val d:Check[VolvoWagon]= new Check[Volvo]() // 用子类指针接了父类
   println("pass check")
 
+  val e:Volvo=new VolvoWagon() // 普通类型的话, 本来就可以用父类指针接子类
 }
